c1cb7c0d586051193411d1f706b60decbe340653
Dominic Meconi <meconidominic@gmail.com>
2024-06-28 18:34:45 -0500

    working

--- googleCardScraper.py
+++ googleCardScraper.py
@@ -1,33 +1,47 @@
 import re
-
-import playwright
 from playwright.sync_api import Page, sync_playwright
-
-import locators
-from locators import *
-from utils import scroll_to_load_data
 from typing import List
 import time
+import locators
+from bs4 import BeautifulSoup
+import requests
+import re
+from locators import *
+from utils import scroll_to_load_data
 
-
-class GoogleBusinessCard():
-    def __init__(self, name=None,review_count=None, rating=None, address=None, website=None, phone=None):
+class GoogleBusinessCard:
+    def __init__(self, name=None, review_count=None, rating=None, address=None, website=None, phone=None, emails=None):
         self.name = name
         self.rating = rating
         self.review_count = review_count
         self.address = address
         self.website = website
         self.phone = phone
+        self.emails = emails or []
 
     def to_dict(self):
         return vars(self)
 
+
+def find_emails(url):
+    response = requests.get(url)
+    data = response.text
+    soup = BeautifulSoup(data, 'html.parser')
+    emails = re.findall('\S+@\S+', soup.text)
+    return emails
+
+url = "http://www.webpage.com"
+print(find_emails(url))
 # Helpers
 def clean_data(value):
-    return re.sub(r'[^a-zA-Z0-9\s()]', '', value)
+    return re.sub(r'[^a-zA-Z0-9\s.\(\)]', '', value)
 
 def endcon(pageed):
     pageed.locator("//span[contains(text(), \"You've reached the end of the list.\")]").is_visible()
+
+def extract_emails(text):
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    return re.findall(email_pattern, text)
 
 
 def googleCardScraper(url, page: Page) -> List[GoogleBusinessCard]:
@@ -37,15 +51,13 @@ def googleCardScraper(url, page: Page) -> List[GoogleBusinessCard]:
     :param page: Browser Instance with already created context
     :return:
     """
-
     # vars
     googleBusinessCards = []
 
     # Logic
-
     page.goto(url, wait_until="load")
 
-    # # pull in all the data through scrolling can remove contains(@class, 'dS8AEf') if needed
+    # pull in all the data through scrolling can remove contains(@class, 'dS8AEf') if needed
     # scroll_to_load_data(page,
     #                     scroll_selector="//*[contains(@class, 'dS8AEf') and @tabindex and @role='feed']",
     #                     endCon=lambda: endcon(page))
@@ -80,42 +92,38 @@ def googleCardScraper(url, page: Page) -> List[GoogleBusinessCard]:
             googleBusinessCard.name = modal.locator("//h1[contains(@class,'DUwDvf')]").inner_text().strip()
         except:
             print("Unable to pull name")
-       # "button", class_ = "CsEnBe"
-        # Get address
-        import re
 
-        # Function to remove non-alphanumeric characters except parentheses
-        def clean_data(value):
-            return re.sub(r'[^a-zA-Z0-9\s()]', '', value)
-
+        # Get phone number
         try:
-            phone_raw = page.locator(
-                "//*[@data-tooltip='Copy phone number' and @class='CsEnBe']").text_content().strip()
+            phone_raw = page.locator("//*[@data-tooltip='Copy phone number' and @class='CsEnBe']").text_content().strip()
             googleBusinessCard.phone = clean_data(phone_raw)
         except:
             print("Unable to pull phone number")
 
+        # Get website
         try:
             website_raw = page.locator("//*[@data-tooltip='Open website' and @class='CsEnBe']").text_content().strip()
             googleBusinessCard.website = clean_data(website_raw)
         except:
             print("Unable to pull website")
 
+        # Get address
         try:
             address_raw = page.locator("//*[@data-tooltip='Copy address' and @class='CsEnBe']").text_content().strip()
             googleBusinessCard.address = clean_data(address_raw)
         except:
             print("Unable to pull address")
 
-        # Get Phone number
+        # Open website and look for emails if website is available
+        if googleBusinessCard.website:
+            print(find_emails(f'http://{googleBusinessCard.website}'))
 
-        # all_info_bars = modal.query_selector_all("button.CsEnBe")
         googleBusinessCards.append(googleBusinessCard)
+
     for gcard in googleBusinessCards:
         print(gcard.to_dict())
     print(googleBusinessCards)
-    return None
-
+    return googleBusinessCards
 
 if __name__ == "__main__":
     """
