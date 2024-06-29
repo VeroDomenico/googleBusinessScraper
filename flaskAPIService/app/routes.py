2650dafff65ecedd94f61ff360d406f352d939a0
Dominic Meconi <meconidominic@gmail.com>
2024-06-29 18:51:53 -0500

    query setup need redis and mongoset

--- flaskAPIService/app/routes.py
+++ flaskAPIService/app/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from .models import Query, QueryStatus
 from . import mongo, redis_client
+import datetime
+
 
 bp = Blueprint('main', __name__)
 
@@ -26,14 +28,30 @@ def create_search_queries():
         if not isinstance(search_queries, list):
             return jsonify({"error": "Invalid data format. 'search_queries' should be a list."}), 400
         
-        response = []
-        for query_string in search_queries:
-            query = Query(search_string=query_string, status=QueryStatus.ADDED)
-            # create redis job
+        created_queries = []
+        for query_data in search_queries:
+            search_string = query_data.get('search_string')
+            if not search_string:
+                return jsonify({"error": "Each query must have 'search_string'."}), 400
 
-            # push to mongoDB last STEP
+            query = Query(
+                search_string=search_string,
+                status=QueryStatus.ADDED,
+                createdTime=datetime.datetime.utcnow(),
+                completedTime=None,
+                numberOfBusinessScraped=0
+            )
+            
+            # Create Redis job
+            redis_task_id = redis_client.rpush('query_jobs', query.to_dict())
+            query.redis_task_id = redis_task_id
+            
+            # Save query to MongoDB
+            query_id = mongo.db.queries_search.insert_one(query.to_dict()).inserted_id
+            query.id = str(query_id)
+            
+            created_queries.append(query.to_dict())
 
-
-        return jsonify(response), 201
+        return jsonify(created_queries), 201
     else:
         return jsonify({"error": "Request must be JSON"}), 400
